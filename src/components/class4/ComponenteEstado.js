8131eb42c857a8e11b4839f5c5aa5043956b7077
Adrian Escalante <adresc@gmail.com>
2022-02-19 11:26:44 -0600

    async mock promise y .map()

--- src/components/class4/ComponenteEstado.js
+++ src/components/class4/ComponenteEstado.js
@@ -5,12 +5,56 @@ const ComponenteEstado = ({nombre}) => {
 
   const [contador, setContador] = useState(0)
 
+  const productos = [
+      {id: '1', name:'Macbook', description: '', stock:'3'},
+      {id: '2', name:'Acer', description: '', stock:'5'},
+      {id: '3', name:'Dell', description: '', stock:'6'},
+      {id: '4', name:'Levono', description: '', stock:'8'}
+  ]
+
+  const nombres = [ 'Ana', 'Juan', 'Maria' ]
+
   useEffect( () => {
+      getProducts()
       console.log('se ejecutó el efecto');
       return () => {
           console.log('Limpieza al unmount');
        }
-  }, [contador] )
+  }, [] )
+
+  const getProducts = () => { 
+      const getProductsPromise = new Promise( (resolve, reject) => {
+        setTimeout(() => {
+            resolve(productos)
+        }, 5000);
+      })
+
+      getProductsPromise.then( response => {
+          console.log(response);
+      })
+   }
+
+//   const promesa = () => {
+//       console.log('Inicia mi funcion con una promesa');
+
+//       const miPromesa = new Promise( (resolve, reject) => {
+//           const random = Math.random()
+//           console.log('random: ', random);
+//           if ( random < 0.5) {
+//               reject('la promesa se rechazó')
+//           } else {
+//               resolve('la promesa se resolvió')
+//           }
+//       })
+
+//       miPromesa.then( (res) => {
+//           console.log(res);
+//       }).catch( e => { 
+//           console.log(e);
+//       }).finally( () => {
+//           console.log('Finally siempre se ejecuta al final');
+//       })
+//    }
 
   const sumarHandler = () => { 
     setContador( contador + 1 )
@@ -30,6 +74,10 @@ const ComponenteEstado = ({nombre}) => {
       {/* <button onClick={() => {setContador(3)} }> + </button> */}
       <button onClick={restarHandler}> - </button>
       <button onClick={sumarHandler}> + </button>
+
+      {nombres.map( (n, i) => <li key={i}><span>{n}</span></li>)}
+
+      {productos.map( producto => <li key={producto.id}>{producto.name} - Stock: {producto.stock}</li> )}
     </>
   )
 }
